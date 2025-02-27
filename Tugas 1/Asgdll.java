de088048109f1a9d5574dddf7175e4603bee9490
aqidah121 <nuraqidahsafanikia@gmail.com>
2025-02-27 21:32:07 +0800

    first git init github

---
+++ Tugas 1/Asgdll.java
@@ -0,0 +1,10 @@
+public class Asgdll {
+ 	public static void main (String[] args) {
+		float f= 20.0f;
+		double fill;
+		
+		fill=10.0f;
+		system.out.println ("f : "+f + "\nfill: "+fill);
+	}
+}
+
